74561bc7d4830cd0182c3c20c5ad9e6ace6296e1
MrcntV <vincent.marconnet@outlook.com>
2025-06-03 17:40:07 +0200

    add

---
+++ serveur/serveur.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 4242;
+
+// Dossier des fichiers buildés
+const staticPath = path.join(__dirname, "build");
+
+app.use(express.static(staticPath));
+
+// Redirection SPA
+app.get("*", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`SPA disponible sur http://localhost:${PORT}`);
+});
