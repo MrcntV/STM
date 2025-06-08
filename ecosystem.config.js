0ee177416146988d2e9e18a9a707d15c90e624b5
MrcntV <vincent.marconnet@outlook.com>
2025-06-08 20:02:47 +0200

    add

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "STM-Server",
+      script: "serveur.js",
+      cwd: "/STM",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+      },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      instances: 1,
+      autorestart: true,
+      max_memory_restart: "300M",
+    },
+  ],
+};
